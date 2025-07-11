e60801cee3e600fbc222225816b851177a7f0d42
Hernan <hayarza@HPC.lan>
2025-07-11 16:06:55 -0600

    added smart comments

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -888,4 +888,50 @@ return {
 			end, { desc = "Format file or range (in visual mode)" })
 		end,
 	},
+	-- Comment - Smart commenting
+	{
+		"numToStr/Comment.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"JoosepAlviste/nvim-ts-context-commentstring",
+		},
+		config = function()
+			local comment = require("Comment")
+
+			-- Enable comment
+			comment.setup({
+				-- Add a space between comment and the line
+				padding = true,
+				-- Whether the cursor should stay at its position
+				sticky = true,
+				-- Lines to be ignored while (un)comment
+				ignore = nil,
+				-- LHS of toggle mappings in NORMAL mode
+				toggler = {
+					line = "gcc", -- Line-comment toggle keymap
+					block = "gbc", -- Block-comment toggle keymap
+				},
+				-- LHS of operator-pending mappings in NORMAL and VISUAL mode
+				opleader = {
+					line = "gc", -- Line-comment keymap
+					block = "gb", -- Block-comment keymap
+				},
+				-- LHS of extra mappings
+				extra = {
+					above = "gcO", -- Add comment on the line above
+					below = "gco", -- Add comment on the line below
+					eol = "gcA", -- Add comment at the end of line
+				},
+				-- Enable keybindings
+				mappings = {
+					basic = true, -- Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+					extra = true, -- Extra mapping; `gco`, `gcO`, `gcA`
+				},
+				-- Function to call before (un)comment
+				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+				-- Function to call after (un)comment
+				post_hook = nil,
+			})
+		end,
+	},
 }
