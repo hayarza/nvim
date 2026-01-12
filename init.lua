c85b5af317516620bf6d76696b2037569a8b2cbe
Hernan Ayarza <hayarza@me.com>
2026-01-12 01:05:56 -0700

    added neo-tree

--- init.lua
+++ init.lua
@@ -46,5 +46,16 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Plugins
 require("lazy").setup({
-  -- we'll add plugins here
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    keys = {
+      { "<leader>e", "<cmd>Neotree toggle<CR>", desc = "File explorer" },
+    },
+  },
 })
